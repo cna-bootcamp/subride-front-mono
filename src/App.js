6c2899548cb58538cbf4c10cee7611b1c497f295
Kimjimin97 <sja9707@daum.net>
2023-12-15 17:29:40 +0900

    cicd test2

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          test cicd
-        </p>
+        <p>test cicd</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React let's go
         </a>
       </header>
     </div>
