d79d7e542fd1778d036338c8546ddf5b1cf76e3d
Kimjimin97 <sja9707@daum.net>
2023-12-18 10:06:37 +0900

    gogo

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React let's go
+          Learn React let's go go
         </a>
       </header>
     </div>
