6931a1e19225d1367b797437ee741c17f239f521
Kimjimin97 <sja9707@daum.net>
2023-12-18 17:56:39 +0900

    :sparkles: 뒤로가기 공통 컴포넌트 추가
    뒤로가기 공통 컴포넌트를 추가했습니다. element props에 text를 입력하면 뒤로가기 header를 사용할 수 있습니다.

---
+++ src/pages/test.jsx
@@ -0,0 +1,12 @@
+import BackHeader from "../commmon/backHeader";
+
+function Test() {
+  return (
+    <>
+      <BackHeader text="뒤로 가기"></BackHeader>
+      <h1>This is test page</h1>
+    </>
+  );
+}
+
+export default Test;
