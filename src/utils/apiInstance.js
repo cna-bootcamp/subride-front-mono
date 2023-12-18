8eebc6ab87d5a82c7eb93c455ca0dc23b8093919
woori9 <wrh95222@gmail.com>
2023-12-18 14:29:47 +0900

    :heavy_plus_sign: React-router-dom 패키지 설치
    리액트 라우팅 설정을 위한 패키지 설치 및 필요없는 파일 삭제

--- src/utils/apiInstance.js
+++ src/utils/apiInstance.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const API_BASE_URL = "http://gudokjoa5.165.192.105.60.nip.io";
 
 const apiInstance = axios.create({
-  baseURL: `${API_BASE_URL}`,
+  baseURL: `${API_BASE_URL}/api`,
   headers: {
     "Content-type": "application/json",
   },
