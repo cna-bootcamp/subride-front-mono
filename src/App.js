71f36d45bfe34fadc1a65c4c5ab108a4bb7b4847
Kimjimin97 <sja9707@daum.net>
2023-12-19 14:54:53 +0900

    :sparkles: 그룹 만들기 페이지 생성
    그룹 만들기 페이지를 만들었습니다. drop 다운을 통해서 그룹 설정을 할 수 있습니다.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Main from "./pages/main";
 import Test from "./pages/test";
+import MakeGroup from "./pages/makeGroup";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Main />} />
       <Route path="/test" element={<Test />} />
+      <Route path="/makeGroup" element={<MakeGroup />} />
     </Routes>
   );
 }
