b34813cec05df724a088b0a5ca6ebf9fbd1597d4
woori9 <wrh95222@gmail.com>
2023-12-19 10:10:55 +0900

    :lipstick: 글로벌 CSS 추가
    공통으로 적용되는 CSS(KB 폰트, 테마 등) 적용

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,5 +1,10 @@
 function Main() {
-  return <h1>This is main page</h1>;
+  return (
+    <>
+      <h1 style={{ fontFamily: "KBFGDisplayB" }}>메인페이지 입니다.</h1>
+      <span>폰트 테스트</span>
+    </>
+  );
 }
 
 export default Main;
