ed3c470021ab1871c4e56048253d747f55248335
Kimjimin97 <sja9707@daum.net>
2023-12-19 19:29:10 +0900

    :lipstick: 그룹 생성 완료 정렬
    그릅 생성 완료 페이지에서 컴포넌트를 ui 정렬을 했습니다. 이후 marginr값과 세부 정렬 작업이 필요합니다.

--- src/common/commonButton.jsx
+++ src/common/commonButton.jsx
@@ -2,15 +2,19 @@ import Button from "@mui/material/Button";
 import { styled } from "@mui/material/styles";
 
 const BootstrapButton = styled(Button)({
-  width: "90%",
+  width: "100%",
   height: "60px",
   boxShadow: "none",
   textTransform: "none",
-  margin: "20px",
+  // margin: "20px",
   fontSize: 20,
   backgroundColor: "#F2DC14",
   color: "#000000",
   borderRadius: 15,
+
+  // position: "absolute",
+  // bottom: "0px",
+
   "&:hover": {
     backgroundColor: "#F2DC14",
     boxShadow: "none",
--- src/pages/successRoom.jsx
+++ src/pages/successRoom.jsx
@@ -2,24 +2,35 @@ import Box from "@mui/material/Box";
 import successMakeRoom from "../assets/successMakeRoom.png";
 import CommonButton from "../common/commonButton";
 import CopyToClipboard from "react-copy-to-clipboard";
+import Button from "@mui/material/Button";
 
 function SuccessRoom() {
   const welcomeCode = "abcde67890123456";
 
   return (
     <>
-      <div>방이 생성 되었습니다.</div>
       <Box
-        component="img"
         sx={{
-          height: 233,
-          width: 350,
-          maxHeight: { xs: 233, md: 167 },
-          maxWidth: { xs: 350, md: 250 },
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
         }}
-        alt="Success Make Room!"
-        src={successMakeRoom}
-      ></Box>
+      >
+        <Box sx={{ width: "100%", textAlign: "center", fontSize: "26px" }}>
+          방이 생성 됐어요
+        </Box>
+        <Box
+          component="img"
+          sx={{
+            height: 233,
+            width: 350,
+          }}
+          alt="Success Make Room!"
+          src={successMakeRoom}
+        ></Box>
+      </Box>
+
+      <div>초대 코드</div>
 
       <div>
         <CopyToClipboard
@@ -29,25 +40,52 @@ function SuccessRoom() {
           <Box
             sx={{
               display: "flex",
+              alignItems: "center",
               justifyContent: "center",
               height: 80,
-              width: "90%",
-              margin: "20px",
-              borderRadius: 15,
+              // width: "90%",
+              // margin: "20px",
+              borderRadius: 5,
               backgroundColor: "#F6F7FA",
-              maxHeight: { xs: 233, md: 167 },
-              maxWidth: { xs: 350, md: 250 },
             }}
           >
-            <Box sx={{ textDecoration: "underline" }}>{welcomeCode}</Box>
+            <Box
+              sx={{
+                textAlign: "center",
+                mr: 3,
+                borderBottom: 1,
+                borderColor: "#AE9C76",
+                fontSize: "20px",
+                color: "#767676",
+              }}
+            >
+              {welcomeCode}
+            </Box>
+
             <div className="URL">
-              <CommonButton text="복사" className="URL"></CommonButton>
+              <Button
+                sx={{
+                  // width: "70px",
+                  height: "50px",
+                  backgroundColor: "#F2DC14",
+                  color: "#000000",
+                  borderRadius: 5,
+                  fontSize: "18px",
+                }}
+                variant="contained"
+                className="URL"
+              >
+                복사
+              </Button>
             </div>
           </Box>
         </CopyToClipboard>
       </div>
 
-      <CommonButton text="확인"></CommonButton>
+      <CommonButton
+        // sx={{ position: "absolute", bottom: "0px" }}
+        text="확인"
+      ></CommonButton>
     </>
   );
 }
