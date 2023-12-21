3a2512c9eddb05adb6e747bdc3b6745ba4c8537d
woori9 <wrh95222@gmail.com>
2023-12-21 16:23:40 +0900

    :necktie: 메인페이지 썹타는 그룹 get API 연결
    메인페이지에서 사용자가 썹타고 있는 그룹을 가져오도록 API 추가

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import bankbookImage from "../assets/bankbook.png";
 import styled from "@emotion/styled";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
@@ -7,103 +7,134 @@ import KeyIcon from "@mui/icons-material/Key";
 import AddIcon from "@mui/icons-material/Add";
 import CustomModal from "../components/main/CustomModal";
 import { Button } from "@mui/material";
+import api from "../utils/apiInstance";
+
+const BankbookContainer = styled.div`
+  text-align: center;
+
+  img {
+    margin: 50px auto 0px;
+    display: block;
+  }
+
+  p {
+    margin: 0px;
+  }
+
+  p.title {
+    margin: 10px 0px 0px 0px;
+    font-size: 24px;
+  }
+
+  div.subtitle-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  p.subtitle {
+    margin: 1px 0px;
+    font-size: 20px;
+  }
+`;
+
+const SubContainer = styled.div`
+  margin-top: 30px;
+  padding: 10px;
+  background-color: #f1f3f5;
+  font-size: 20px;
+  width: 100%;
+
+  .display-flex {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  p {
+    margin: 0px;
+  }
+
+  .images {
+    list-style: none;
+    padding: 0;
+    margin-top: 20px;
+    overflow-x: auto;
+    white-space: nowrap;
+  }
+
+  .image-box {
+    display: inline-block;
+    width: 70px;
+    height: 70px;
+    border-radius: 70%;
+    background-color: pink;
+    margin: 7px 14px 0px 0px;
+    object-fit: cover;
+
+    p {
+      font-size: 10px;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+      word-break: break-all;
+      text-align: center;
+    }
+  }
+
+  .image-profile {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  .image-box-recommend {
+    display: inline-block;
+    width: 90px;
+    height: 120px;
+    border-radius: 5px;
+    background-color: pink;
+    margin: 7px;
+  }
+`;
+
+const ModalContent = styled.div`
+  text-align: center;
+
+  p {
+    margin: 3%;
+    font-size: 15px;
+  }
+
+  .button-box {
+    display: flex;
+    padding: 3%;
+  }
+`;
+
+const getGroupList = async (userId) => {
+  try {
+    const { data } = await api.get("/group/mylist", {
+      params: { id: userId },
+    });
+    return data;
+  } catch (err) {
+    return err;
+  }
+};
 
 function Main({ user }) {
+  const [subGroupList, setSubGroupList] = useState([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const closeModal = () => {
     setIsModalOpen(false);
   };
 
-  const BankbookContainer = styled.div`
-    text-align: center;
-
-    img {
-      margin: 50px auto 0px;
-      display: block;
-    }
-
-    p {
-      margin: 0px;
-    }
-
-    p.title {
-      margin: 10px 0px 0px 0px;
-      font-size: 24px;
-    }
-
-    div.subtitle-container {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-    }
-
-    p.subtitle {
-      margin: 1px 0px;
-      font-size: 20px;
-    }
-  `;
-
-  const SubContainer = styled.div`
-    margin-top: 30px;
-    padding: 10px;
-    background-color: #f1f3f5;
-    font-size: 20px;
-    width: 100%;
-
-    .display-flex {
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-    }
-
-    p {
-      margin: 0px;
-    }
-
-    .images {
-      margin-top: 20px;
-      overflow-x: auto;
-      white-space: nowrap;
-    }
-
-    .image-box {
-      display: inline-block;
-      width: 70px;
-      height: 70px;
-      border-radius: 70%;
-      background-color: pink;
-      margin: 7px;
-    }
-
-    .image-profile {
-      width: 100%;
-      height: 100%;
-      object-fit: cover;
-    }
-
-    .image-box-recommend {
-      display: inline-block;
-      width: 90px;
-      height: 120px;
-      border-radius: 5px;
-      background-color: pink;
-      margin: 7px;
-    }
-  `;
-
-  const ModalContent = styled.div`
-    text-align: center;
-
-    p {
-      margin: 3%;
-      font-size: 15px;
-    }
-
-    .button-box {
-      display: flex;
-      padding: 3%;
-    }
-  `;
+  useEffect(() => {
+    getGroupList(user.id).then((result) => {
+      setSubGroupList(result);
+    });
+  }, [user.id]);
 
   return (
     <>
@@ -123,19 +154,21 @@ function Main({ user }) {
             <AddIcon sx={{ fontSize: "25px", color: "#F2DC14" }} />
           </Button>
         </div>
-        <div className="images">
-          <div className="image-box"></div>
-          <div className="image-box"></div>
-          <div className="image-box"></div>
-          <div className="image-box"></div>
-          <div className="image-box"></div>
-          <div className="image-box"></div>
-          <div className="image-box"></div>
-          <div className="image-box"></div>
-          <div className="image-box"></div>
-          <div className="image-box"></div>
-          <div className="image-box"></div>
-        </div>
+        <ul className="images">
+          {subGroupList.map((item) => (
+            <li key={item.id} className="image-box">
+              <img
+                className="image-profile"
+                src={
+                  process.env.PUBLIC_URL +
+                  `/service/${item.subscribeDTO.serviceId}.png`
+                }
+                alt={item.subscribeDTO.serviceId}
+              />
+              <p>{item.subscribeDTO.serviceName}</p>
+            </li>
+          ))}
+        </ul>
       </SubContainer>
 
       <SubContainer>
